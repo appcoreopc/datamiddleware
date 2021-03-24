9a6c5df180241aa1010d29a14bb193eb4e8e4a8f
appcoreopc <jeremy@jeremys-MacBook-Pro.local>
2021-03-24 13:44:31 +0800

    added covid notifice providers

---
+++ src/providers/covidNoticeDataProvider.ts
@@ -0,0 +1,20 @@
+
+class CovidNoticeDataProvider {
+    
+    constructor(private _dbClient: any)
+    {    
+
+    }
+    
+    async get() {
+    
+    }
+    
+    async add() {
+    }
+    
+    async update() {
+        
+    }
+    
+}
--- src/resolvers/index.ts
+++ src/resolvers/index.ts
@@ -12,8 +12,9 @@ const books = [
   },
 ];
 
-
 const prisma = new PrismaClient()
+const travelNoticeProvider = new TravelNoticeDataProvider(prisma);
+const covidNoticeProvider = new CovidNoticeDataProvider(prisma);
 
 export const resolvers = {
   Query: {
